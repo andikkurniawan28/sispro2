7b09f30cc0b249b854ac3796efad1de20de322d4
Andik Kurniawan <andikk362@gmail.com>
2024-07-18 15:30:26 +0700

    Persiapan modul Akuntansi

---
+++ app/Http/Controllers/AkunDasarController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AkunDasar;
+use Illuminate\Http\Request;
+
+class AkunDasarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AkunDasar $akunDasar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AkunDasar $akunDasar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AkunDasar $akunDasar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AkunDasar $akunDasar)
+    {
+        //
+    }
+}
---
+++ app/Models/AkunSub.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AkunSub extends Model
+{
+    use HasFactory;
+}
